a5f2a3cb67bf2a6d43be4c27b4d3d4a435e36897
guido.lomanto@gmail.com <guido.lomanto@gmail.com@f9b496d8-eb72-d83f-fbd0-2c307065cde9>
2011-09-05 00:39:04 +0000

    Ejemplo de MDB que envia mensajes, no se si funciona con el JMS client, ahora me pongo a verlo pero como la clase es inocua y no rompe nada, me parecio mejor subirla al SVN por cualquier cosa

--- CasaCentralEJB/ejbModule/uade/server/beans/mdb/EnviarSolDistMessage.java
+++ CasaCentralEJB/ejbModule/uade/server/beans/mdb/EnviarSolDistMessage.java
@@ -5,6 +5,7 @@ import java.util.Hashtable;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.DeliveryMode;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.Queue;
@@ -15,6 +16,10 @@ import javax.jms.QueueSession;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 
 //Especifico tipo y nombre logico de la cola de destino
 @MessageDriven(activationConfig = {
@@ -25,35 +30,46 @@ public class EnviarSolDistMessage implements MessageListener {
 	public void onMessage(Message arg0) {
 
 		// get the initial context
-		/*PropertiesConfiguration config = new PropertiesConfiguration("config.properties");
-		Hashtable<String, String> props = new Hashtable<String,String>();
-		props.put(InitialContext.INITIAL_CONTEXT_FACTORY,"org.jnp.interfaces.NamingContextFactory");
-		props.put(InitialContext.PROVIDER_URL,config.getString("naming.url"));
-		InitialContext ic;
-		InitialContext ctx = new InitialContext(props);
-
-		// lookup the queue object
-		Queue queue = (Queue) ctx.lookup("queue/queueLC1");
-		// lookup the queue connection factory
-		QueueConnectionFactory connFactory = (QueueConnectionFactory) ctx.lookup("queue/connectionFactory");
-
-		// create a queue connection
-		QueueConnection queueConn = connFactory.createQueueConnection();
-		// create a queue session
-		QueueSession queueSession = queueConn.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
-		// create a queue sender
-		QueueSender queueSender = queueSession.createSender(queue);
-		queueSender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-
-		// create a simple message to say "Hello"
-		TextMessage message = queueSession.createTextMessage("Hello");
-		// send the message
-		queueSender.send(message);
-		// print what we did
-		System.out.println("sent: " + message.getText());
-
-		// close the queue connection
-		queueConn.close();*/
+		PropertiesConfiguration config;
+		try {
+			config = new PropertiesConfiguration("config.properties");
+			Hashtable<String, String> props = new Hashtable<String,String>();
+			props.put(InitialContext.INITIAL_CONTEXT_FACTORY,"org.jnp.interfaces.NamingContextFactory");
+			props.put(InitialContext.PROVIDER_URL,config.getString("naming.url"));
+			InitialContext ctx = new InitialContext(props);
+			
+			// lookup the queue object
+			Queue queue = (Queue) ctx.lookup("queue/queueLC1");
+			// lookup the queue connection factory
+			QueueConnectionFactory connFactory = (QueueConnectionFactory) ctx.lookup("queue/connectionFactory");
+			
+			// create a queue connection
+			QueueConnection queueConn = connFactory.createQueueConnection();
+			// create a queue session
+			QueueSession queueSession = queueConn.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
+			// create a queue sender
+			QueueSender queueSender = queueSession.createSender(queue);
+			queueSender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+			
+			// create a simple message to say "Hello"
+			TextMessage message = queueSession.createTextMessage("Hello");
+			// send the message
+			queueSender.send(message);
+			// print what we did
+			System.out.println("sent: " + message.getText());
+			
+			// close the queue connection
+			queueConn.close();
+		} catch (ConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
