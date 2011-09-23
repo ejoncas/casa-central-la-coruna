591fe401a53a97a785f74dd8dc0553f18aac11b7
guido.lomanto@gmail.com <guido.lomanto@gmail.com@f9b496d8-eb72-d83f-fbd0-2c307065cde9>
2011-09-23 13:08:52 +0000

    Se elimino el tag GeneratedId, se agregaron constructores para ser utilizados desde TestUtils y poder asignar un ID especifico a las tiendas y centros de distribucion.

--- CasaCentralWeb/test/is2/uade/test/TestUtils.java
+++ CasaCentralWeb/test/is2/uade/test/TestUtils.java
@@ -46,14 +46,14 @@ public class TestUtils {
 	public static List<CentroDistribucionDTO> obtenerCentros(){
 		List<CentroDistribucionDTO> centros = new ArrayList<CentroDistribucionDTO>();
 		//TORTUGUITAS
-		centros.add(new CentroDistribucionDTO( 34.549549,58.573952,"TORTUGUITAS (GRUPO 11)"));
+		centros.add(new CentroDistribucionDTO(11L, 34.549549,58.573952,"TORTUGUITAS (GRUPO 11)"));
 		//UNICENTER
-		CentroDistribucionDTO unicenter = new CentroDistribucionDTO( 34.50203,58.52108,"UNICENTER (GRUPO 6)");
+		CentroDistribucionDTO unicenter = new CentroDistribucionDTO(6L,34.50203,58.52108,"UNICENTER (GRUPO 6)");
 		centros.add(unicenter);
 		//PUNTA CARRETAS
-		centros.add(new CentroDistribucionDTO( 34.927601,56.160393,"PUNTA CARRETAS (GRUPO 5)"));
+		centros.add(new CentroDistribucionDTO(5L,34.927601,56.160393,"PUNTA CARRETAS (GRUPO 5)"));
 		//MONTEVIDEO
-		centros.add(new CentroDistribucionDTO( 34.890578,56.155071,"MONTEVIDEO (GRUPO 10)"));
+		centros.add(new CentroDistribucionDTO(10L,34.890578,56.155071,"MONTEVIDEO (GRUPO 10)"));
 		//PARQUE ARAUCO
 		//centros.add(new CentroDistribucionDTO(33.46775,70.749872,"PARQUE ARAUCO"));
 		//MARINA CENTER
@@ -65,19 +65,19 @@ public class TestUtils {
 	public static List<TiendaDTO> obtenerTiendas() {
 		List<TiendaDTO> tiendas = new ArrayList<TiendaDTO>();
 		//ARGENTINA
-		tiendas.add(new TiendaDTO("Tortuguitas (GRUPO 4)",34.50981 ,58.573980));
-		tiendas.add(new TiendaDTO("Unicenter (GRUPO 8)",34.433036,58.520935));
+		tiendas.add(new TiendaDTO(4L,"Tortuguitas (GRUPO 4)",34.50981 ,58.573980));
+		tiendas.add(new TiendaDTO(8L,"Unicenter (GRUPO 8)",34.433036,58.520935));
 	/*	tiendas.add(new TiendaDTO("Santa Fé",34.551811,58.417225));
 		tiendas.add(new TiendaDTO("Abasto",34.611807,58.417311));
 		tiendas.add(new TiendaDTO("Florida",34.61163,58.38418));
 		tiendas.add(new TiendaDTO("Paseo Alcorta",34.575154,58.404243));*/
 		
 		//URUGUAY
-		tiendas.add(new TiendaDTO("Punta Carretas (GRUPO 3)",34.919086,56.163054));
+		tiendas.add(new TiendaDTO(3L,"Punta Carretas (GRUPO 3)",34.919086,56.163054));
 		//tiendas.add(new TiendaDTO("Montevideo Shopping",34.88931,56.058576));
 		
 		//CHILE
-		tiendas.add(new TiendaDTO("Parque Arauco (GRUPO 9)", 33.465924, 70.655909));
+		tiendas.add(new TiendaDTO(9L,"Parque Arauco (GRUPO 9)", 33.465924, 70.655909));
 		//tiendas.add(new TiendaDTO("Marina Center",33.389026,70.714874));
 		
 		/*tiendas.add(new TiendaDTO("Alto Las Condes",33.46912,70.641997));
